6ef43e23f8facaf4d1d11be3b2817e856ffc16a5
Bianca Alverne <127367150+BiaAlverne@users.noreply.github.com>
2024-06-01 11:33:57 -0300

    imc.C

--- imc.C
+++ imc.C
@@ -5,7 +5,7 @@ int main() {
     float imc, peso, altura;
 
     printf("Digite seu nome: ");
-    scanf("%s", nome);
+    scanf("%99[^\n]", nome);  // Lê a linha inteira, incluindo espaços
    
     printf("Digite a sua altura em metros: ");
     scanf("%f", &altura);
@@ -17,13 +17,10 @@ int main() {
     
     if (imc < 18.5)
         printf("Olá, %s. Você está abaixo do peso.\n", nome);
-    
-    else if (imc >= 18.5 && imc <= 24.9)
+    else if (imc >= 18.5 && imc < 25.0)  // Ajuste da comparação
         printf("Olá, %s. Você está no seu peso ideal.\n", nome);
-    
-    else if (imc >= 25 && imc < 29.9)
+    else if (imc >= 25.0 && imc < 30.0)  // Ajuste da comparação
         printf("Olá, %s. Você está acima do peso.\n", nome);
-    
     else
         printf("Olá, %s. Você está obeso.\n", nome);
 
